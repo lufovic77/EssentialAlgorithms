e9db0e3f5823cb7bb1658870ececab2459504d0a
lufovic77 <lufovic77@gmail.com>
2020-05-08 10:42:17 +0000

    rod cutting problem description

---
+++ dynamicProgramming/rodCutting.cpp
@@ -0,0 +1,6 @@
+/* Given a rod of length n inches 
+ * and a price per inches array P, 
+ * compute the maximum revenue by cutting the rod.
+ * Not cutting the rod can also be the optimal solution. 
+ * */
+
