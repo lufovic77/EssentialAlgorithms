38bbd38d6f91f40dfb7ed73ed109bfdcee29bc5d
lufovic77 <lufovic77@gmail.com>
2020-05-08 10:49:08 +0000

    add description

--- dynamicProgramming/rodCutting.cpp
+++ dynamicProgramming/rodCutting.cpp
@@ -2,5 +2,21 @@
  * and a price per inches array P, 
  * compute the maximum revenue by cutting the rod.
  * Not cutting the rod can also be the optimal solution. 
+ * 
+ * <Solution>
+ * Rod length | Answer | Solution 
+ * 1			1		 1
+ * 2			5		 2
+ * 3			8		 3
+ * 4			10		 2+2
+ * 5			13		 2+3
+ * 6			17		 6
+ * 7			18		 1+6 or 2+2+3
+ * 8			22		 2+6
  * */
+#include <iostream>
+using namespace std;
+int P[11] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+int main(){
 
+}
